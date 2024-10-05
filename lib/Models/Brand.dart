88714ccd51b7de8be427a62b006addeb7933c774
Rahaf0B <rahafbakeer123@gmail.com>
2024-10-05 21:31:14 +0300

    clear all the folders and delete the codes

--- lib/Models/Brand.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ecommerce/Models/Figure.dart';
-
-class Brand {
-  Brand({required this.brand_id, required this.name, required this.image});
-  final int brand_id;
-  final String name;
-  final Figure image;
-}
